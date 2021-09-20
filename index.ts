60274cecc8683bc847d9a98639e9ee57ab5cc1fd
Mehmet Sönmez <73697537+mhmtsnmzkanly@users.noreply.github.com>
2021-09-20 16:11:06 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,30 +2,48 @@ import { Harmony } from "./Deps.ts";
 import * as Config from "./Config.ts";
 import * as Commands from "./Commands/List.ts";
 
+/*
+*
+* Bot Ayarları
+*
+*/
 // Oluştur ve ayarla
 const client = new Harmony.CommandClient({
   prefix: Config.Bot.Prefix,
   caseSensitive: false,
 });
 
-/* Commandları */
-// Örnekleri Ekle
-client.commands.add(Commands.x);
-client.commands.add(Commands.y);
-client.commands.add(Commands.z);
+// Bot Ayarları
+/*
+client.editUser({
+  username: Config.Bot.Name,
+  avatar: Config.Bot.ProfilePhoto,
+});
+*/
 
-// Günlük Mesajlar
-client.commands.add(Commands.CommandGunaydin);
-client.commands.add(Commands.CommandSelamlama);
-client.commands.add(Commands.CommandDokunmayin);
-
-// Bilgilendirme & Bota bağlanma
+// Bot Hazırsa:
 client.on("ready", () => {
+  // Ekrana yazdır
   console.log(
     `Bot Hazır ve Sistem Çalışır Durumda!\nBot: ${client.user?.tag}`,
   );
 });
 
+/*
+*
+* Komutlar
+*
+*/
+// Örnekleri Ekle
+client.commands.add(Commands.x);
+client.commands.add(Commands.y);
+client.commands.add(Commands.z);
+// Günlük Mesajlar
+client.commands.add(Commands.CommandGunaydin);
+client.commands.add(Commands.CommandSelamlama);
+client.commands.add(Commands.CommandDokunmayin);
+
+// Bota bağlan
 client.connect(Config.Bot.Token, [
   Harmony.GatewayIntents.GUILD_MESSAGES,
   Harmony.GatewayIntents.GUILDS,
