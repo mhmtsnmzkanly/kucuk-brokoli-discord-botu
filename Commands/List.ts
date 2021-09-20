78d334123cbd3d9e692b115be4ab53e0a84b675b
Mehmet Sönmez <73697537+mhmtsnmzkanly@users.noreply.github.com>
2021-09-20 15:19:10 +0300

    Update List.ts

--- Commands/List.ts
+++ Commands/List.ts
@@ -4,4 +4,3 @@ export { z } from "./z.ts";
 export { CommandGunaydin } from "./CommandGunaydin.ts";
 export { CommandSelamlama } from "./CommandSelamlama.ts";
 export { CommandDokunmayin } from "./CommandDokunmayin.ts";
-export { CommandTeknikSorun } from "./CommandTeknikSorun.ts";
