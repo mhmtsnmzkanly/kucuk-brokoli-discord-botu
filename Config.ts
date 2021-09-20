4861e92f8fb9a54ec4c9ab5ab70f9fb5a672974e
Mehmet Sönmez <73697537+mhmtsnmzkanly@users.noreply.github.com>
2021-09-20 16:11:42 +0300

    Update Config.ts

--- Config.ts
+++ Config.ts
@@ -1,6 +1,8 @@
 import "https://deno.land/x/dotenv@v0.4.1/load.ts";
 
 const Bot = {
+  Name: String(Deno.env.get("BOT_NAME")),
+  ProfilePhoto: String(Deno.env.get("BOT_PP")),
   Prefix: String(Deno.env.get("BOT_PREFIX")),
   Token: String(Deno.env.get("BOT_TOKEN")),
 };
